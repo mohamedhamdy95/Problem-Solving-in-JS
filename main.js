6ff04b9b5d57ab319cdc74022f98d898a0786ddd
mohamedhamdy95 <mohamed.hamdy999@gmail.com>
2024-08-19 11:24:54 +0300

    first

---
+++ main.js
@@ -0,0 +1,18 @@
+// write func that take 2 params
+// first paramter is string of words
+// second paramter is character
+// calculate the number of char that apper in the string
+
+// function countChar(str, char) {
+//     counter = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] == char) {
+//             counter++;
+//         }
+//     }
+//     console.log(counter);
+// }
+// countChar("DDDSSR", "V");
+// =================================================
+
+
